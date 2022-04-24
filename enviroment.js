f1dbaa263c72ffe12ed2cc3a19230625d6c60149
BISWAS <71728982+rupanbiswas@users.noreply.github.com>
2022-04-24 21:50:57 +0530

    Update enviroment.js

--- enviroment.js
+++ enviroment.js
@@ -1,4 +1,4 @@
-module.exports.bucket_name = 'melio-file';
+module.exports.bucket_name = '-file';
 // module.exports.bucket_region = 'us-east-2';
-module.exports.secretAccessId = 'AKIAYSJF277XD6X6PT6L';
-module.exports.secretAccessKey = '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';
+module.exports.secretAccessId = '';
+module.exports.secretAccessKey = '+Icsh6J0ldYHqomA8cCVl/q8v';

4f9cc6070695a28fa89af5643404349366f5360d
Rupan Biswas <71728982+rupanbiswas@users.noreply.github.com>
2022-11-24 16:13:04 +0530

    Update enviroment.js

--- enviroment.js
+++ enviroment.js
@@ -1,4 +1,4 @@
-module.exports.bucket_name = '-file';
-// module.exports.bucket_region = 'us-east-2';
-module.exports.secretAccessId = '';
-module.exports.secretAccessKey = '+Icsh6J0ldYHqomA8cCVl/q8v';
+module.exports.bucket_name = 
+// module.exports.bucket_region = 
+module.exports.secretAccessId = 
+module.exports.secretAccessKey = 
